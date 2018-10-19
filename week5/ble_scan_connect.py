eff855d5bb197cf93082f5df89fcad425a34c183
TeresaChou <chowduen@gmail.com>
2018-10-19 17:41:46 +0800

    .

--- week5/ble_scan_connect.py
+++ week5/ble_scan_connect.py
@@ -10,14 +10,18 @@ class ScanDelegate(DefaultDelegate):
             print "Discovered device", dev.addr
         elif isNewData:
             print "Received new data from", dev.addr
+    def setLED(self, led1, led2):
+	self.led1 = led1
+	self.led2 = led2
     def setHandle(self,ch1, ch2):
 	self.handle1 = ch1.getHandle()
 	self.handle2 = ch2.getHandle()
     def handleNotification(self, cHandle, data):
-        print "call back"
-	if(cHandle == self.handle1 or cHandle == self.handle2):
-	    print "printing data"
-            print (data,"123")
+        print "notice!"
+	if(cHandle == self.handle1):
+	    led1.write(data)
+	else if(cHandle == self.handle2):
+	    led2.write(data)
 
 deleg = ScanDelegate()
 ##scanner = Scanner().withDelegate(deleg)
@@ -63,6 +67,7 @@ ch_led2 = srv_l.getCharacteristics(LED2)[0]
 ch_but1 = srv_b.getCharacteristics(BUT1)[0]
 ch_but2 = srv_b.getCharacteristics(BUT2)[0]
 
+deleg.setLED(ch_led1, ch_led2)
 deleg.setHandle(ch_but1, ch_but2)
 dev.writeCharacteristic(deleg.handle1, '\x01\x00')
 print deleg.handle1
@@ -79,10 +84,10 @@ try:
 
     while True:
 	if dev.waitForNotifications(3.0):
-            print "get notification"
+            # print "get notification"
 	    continue
         else:
-            print "no note detected"
+            # print "no note detected"
 
 finally:
     dev.disconnect()
